d3d652948fa7fd7d871e7c0f7e484d5cfc1e927b
slikts <dabas@untu.ms>
2014-03-17 06:09:50 +0200

    .parseHTML, refactor

--- ecmaquery.js
+++ ecmaquery.js
@@ -2,7 +2,6 @@
 
 //(function() {
 var $ = ecmaQuery;
-var tempBlock = document.createElement('div');
 var dataMap = new Map();
 var proto;
 
@@ -11,7 +10,7 @@ function ecmaQuery(selector, context) {
 }
 
 // core
-proto = $.prototype = {
+proto = $.fn = $.prototype = {
   constructor: $,
   length: 0,
   init: function(x, context) {
@@ -22,9 +21,7 @@ proto = $.prototype = {
     }
     if (typeof x === 'string') {
       if (x[0] === '<') {
-        tempBlock.innerHTML = x;
-        elems = $.clone(tempBlock.children);
-        elems.forEach(tempBlock.removeChild, tempBlock);
+        elems = $.parseHTML(x);
       } else {
         elems = $.find(x, context);
       }
@@ -128,8 +125,9 @@ proto = $.prototype = {
     // XXX handle functions, collections and elements
     return !!$.filterMatches(this, x).length;
   },
-  add: function(selector, context) {
-    return this.pushStack($.merge(this, $.find(selector, context)));
+  add: function(x, context) {
+    // XXX
+    return this.pushStack($.merge(this, $.find(x, context)));
   },
   index: function(x) {
     var el;
@@ -168,34 +166,12 @@ proto = $.prototype = {
     return this.pushStack(this);
   },
   splice: Array.prototype.splice
-      //parent
-      //parents
-      //parentsUntil
-      //nextUntil
-      //prevUntil
-      //nextAll
-      //prevAll
-      //children
-      //contents
-      //siblings
-
-      //append
-      //prepend
-      //before
-      //after
-      //text
-      //empty
-      //clone
-      //html
-      //replaceWith
-
-      //css
 };
 
 
 // dom methods
 Object.assign(proto, (function() {
-  function _eachProp(name, data) {
+  function _setProp(name, data) {
     if (data === undefined) {
       return this.length ? this[0][name] : undefined;
     }
@@ -206,24 +182,24 @@ Object.assign(proto, (function() {
     return undefined;
   }
 
-  function walkUp(el, callback) {
-    var node = el.parentNode;
-    if (callback(node)) {
-      walkUp(node, callback);
+  function walk(el, prop, callback) {
+    var node = el[prop];
+    if (node && callback(node)) {
+      walk(node, prop, callback);
     }
   }
 
   return {
     html: function html(data) {
-      return _eachProp.call(this, 'innerHTML', data) || this;
+      return _setProp.call(this, 'innerHTML', data) || this;
     },
     text: function text(data) {
-      return _eachProp.call(this, 'innerText', data) || this;
+      return _setProp.call(this, 'innerText', data) || this;
     },
     closest: function(selector, context) {
       var matches = [];
       $.each(this, function(el) {
-        walkUp(el, function(node) {
+        walk(el, 'parentNode', function(node) {
           if (node === context || node === document) {
             return false;
           }
@@ -235,7 +211,11 @@ Object.assign(proto, (function() {
           return true;
         });
       });
+
       return this.pushStack(matches, context);
+    },
+    append: function() {
+      var elems = $.flatten(arguments);
     },
     parent: function(selector) {
       var elems = $.map($.toArray(this), function(el) {
@@ -252,7 +232,18 @@ Object.assign(proto, (function() {
 
 // dom utils
 Object.assign($, (function() {
+  var tempBlock = document.createElement('div');
+
   return {
+    parseHTML: function(html) {
+      var elems;
+
+      tempBlock.innerHTML = html;
+      elems = $.toArray(tempBlock.children);
+      elems.forEach(tempBlock.removeChild, tempBlock);
+
+      return elems;
+    },
     find: function(selector, context) {
       if (!context) {
         context = [document];
@@ -297,257 +288,14 @@ Object.assign($, (function() {
           return undefined;
         }
         return fn.call(el, selector);
-      };
+      }
+      ;
 
       return matchesSelector;
     })()
   };
 })());
 
-// array/object utils
-Object.assign($, (function() {
-  var objKeys = Object.keys;
-  var objAssign = Object.assign;
-  var objCreate = Object.create;
-  var objGetPrototypeOf = Object.getPrototypeOf;
-  var objGetOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
-  var objGetOwnPropertyNames = Object.getOwnPropertyNames;
-
-  var arrProto = Array.prototype;
-  var arrIsArray = Array.isArray;
-  var arrForEach = arrProto.forEach;
-  var arrMap = arrProto.map;
-  var arrReduce = arrProto.reduce;
-  var arrFilter = arrProto.filter;
-  var arrEvery = arrProto.every;
-  var arrSome = arrProto.some;
-  var arrIndexOf = arrProto.indexOf;
-  var arrSlice = arrProto.slice;
-  var arrConcat = arrProto.concat;
-
-  function indexOf(obj, x) {
-    return arrIndexOf.call(obj, x);
-  }
-
-  function every(obj, callback, thisArg) {
-    if (isArrayLike(obj)) {
-      return arrEvery.call(obj, callback, thisArg);
-    }
-
-    return objKeys(obj).every(function(key) {
-      return callback.call(thisArg, obj[key], key, obj);
-    });
-  }
-
-  function some(obj, callback, thisArg) {
-    if (isArrayLike(obj)) {
-      return arrSome.call(obj, callback, thisArg);
-    }
-
-    return objKeys(obj).some(function(key) {
-      return callback.call(thisArg, obj[key], key, obj);
-    });
-  }
-
-  function each(obj, callback, thisArg) {
-    if (isArrayLike(obj)) {
-      return arrForEach.call(obj, callback, thisArg);
-    }
-
-    return arrForEach.call(objKeys(obj), function(key) {
-      callback.call(thisArg, obj[key], key, obj);
-    }, thisArg);
-  }
-
-  function map(obj, callback, thisArg) {
-    if (isArrayLike(obj)) {
-      return arrMap.call(obj, callback, thisArg);
-    }
-
-    var ret = clone(obj);
-
-    each(objKeys(ret), function(value, key) {
-      ret[key] = callback.apply(thisArg, arguments);
-    }, thisArg);
-
-    return ret;
-  }
-
-  function extend() {
-    return arrReduce.call(arguments, function(target, source) {
-      return objAssign(target, source);
-    });
-  }
-
-  function filter(obj, callback, thisArg) {
-    callback = callback || function(value) {
-      return value;
-    };
-
-    if (obj.length) {
-      return arrFilter.call(obj, callback, thisArg);
-    }
-
-    var ret = objCreate(objGetPrototypeOf(obj));
-
-    each(objKeys(obj).filter(function(key) {
-      return callback.call(thisArg, obj[key], key, obj);
-    }), function(key) {
-      ret[key] = obj[key];
-    });
-
-    return ret;
-  }
-
-  // array-like only
-  function unique(obj) {
-    return arrFilter.call(obj, function(val, i, arr) {
-      return (i <= arrIndexOf.call(arr, val));
-    });
-  }
-
-  function clone(obj, deep, callback) {
-    if (!(obj instanceof Object)) {
-      return obj;
-    }
-
-    var descriptors = {};
-
-    objGetOwnPropertyNames(obj).forEach(function(name) {
-      var prop = objGetOwnPropertyDescriptor(obj, name);
-
-      if (deep) {
-        prop.value = $.clone(prop.value);
-      }
-
-      descriptors[name] = prop;
-    });
-
-    return objCreate(objGetPrototypeOf(obj), descriptors);
-  }
-
-  function _posMod(a, b) {
-    a = a % b;
-    if (a < 0) {
-      a += b;
-    }
-    return a;
-  }
-
-  function get(obj, i) {
-    if (!isArrayLike(obj)) {
-      obj = objKeys(obj).sort();
-    }
-    return obj[_posMod(i, obj.length)];
-  }
-
-  function isArrayLike(obj) {
-    return obj.length !== undefined;
-  }
-
-  function last(obj) {
-    return get(obj, -1);
-  }
-
-  function first(obj) {
-    return get(obj, 0);
-  }
-
-  // array-like only
-  function flatten(obj, deep) {
-    var ret = [];
-
-    arrForEach.call(obj, function(value) {
-      arrForEach.call(value, function(subValue) {
-        ret.push(subValue);
-      });
-    });
-
-    return ret;
-  }
-
-  function values(obj) {
-    return objKeys(obj).map(function(key) {
-      return obj[key];
-    });
-  }
-
-  // XXX
-  function toArrayLike(obj) {
-    if (isArrayLike(obj)) {
-      return obj;
-    }
-
-    var ret = objCreate(null);
-
-    ret[0] = obj;
-    ret.length = 1;
-
-    return ret;
-  }
-
-  function toArray(obj) {
-    return arrSlice.call(obj, 0);
-  }
-
-  // XXX
-  function bindRight() {
-    var self = this;
-    var args = arrProto.slice.call(arguments);
-    return function() {
-      return self.apply(this, arrProto.slice.call(arguments).concat(args));
-    };
-  }
-
-  // array-like only
-  function difference(arr) {
-    var rest = flatten(arrSlice.call(arguments, 1));
-
-    return $.filter(arr, function(value) {
-      return !contains(rest, value);
-    });
-  }
-
-  // array-like only
-  function contains(arr, item) {
-    return ~arrIndexOf.call(arr, item);
-  }
-
-  // array-like only
-  function merge() {
-    return arrConcat.apply([], map(arguments, function(value) {
-      if (!arrIsArray(value) && isArrayLike(value)) {
-        value = toArray(value);
-      }
-      return value;
-    }));
-  }
-
-  return {
-    every: every,
-    some: some,
-    map: map,
-    each: each,
-    clone: clone,
-    unique: unique,
-    filter: filter,
-    extend: extend,
-    last: last,
-    first: first,
-    get: get,
-    flatten: flatten,
-    isArrayLike: isArrayLike,
-    toArrayLike: toArrayLike,
-    toArray: toArray,
-    values: values,
-    bindRight: bindRight,
-    indexOf: indexOf,
-    difference: difference,
-    contains: contains,
-    merge: merge
-  };
-})());
-
-window.$ = $;
+//window.$ = $;
 
 //})();
---
+++ util.js
@@ -0,0 +1,243 @@
+// array/object utils
+Object.assign($, (function() {
+  var objKeys = Object.keys;
+  var objAssign = Object.assign;
+  var objCreate = Object.create;
+  var objGetPrototypeOf = Object.getPrototypeOf;
+  var objGetOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+  var objGetOwnPropertyNames = Object.getOwnPropertyNames;
+
+  var arrProto = Array.prototype;
+  var arrIsArray = Array.isArray;
+  var arrForEach = arrProto.forEach;
+  var arrMap = arrProto.map;
+  var arrReduce = arrProto.reduce;
+  var arrFilter = arrProto.filter;
+  var arrEvery = arrProto.every;
+  var arrSome = arrProto.some;
+  var arrIndexOf = arrProto.indexOf;
+  var arrSlice = arrProto.slice;
+  var arrConcat = arrProto.concat;
+
+  function indexOf(obj, x) {
+    return arrIndexOf.call(obj, x);
+  }
+
+  function every(obj, callback, thisArg) {
+    if (isArrayLike(obj)) {
+      return arrEvery.call(obj, callback, thisArg);
+    }
+
+    return objKeys(obj).every(function(key) {
+      return callback.call(thisArg, obj[key], key, obj);
+    });
+  }
+
+  function some(obj, callback, thisArg) {
+    if (isArrayLike(obj)) {
+      return arrSome.call(obj, callback, thisArg);
+    }
+
+    return objKeys(obj).some(function(key) {
+      return callback.call(thisArg, obj[key], key, obj);
+    });
+  }
+
+  function each(obj, callback, thisArg) {
+    if (isArrayLike(obj)) {
+      return arrForEach.call(obj, callback, thisArg);
+    }
+
+    return arrForEach.call(objKeys(obj), function(key) {
+      callback.call(thisArg, obj[key], key, obj);
+    }, thisArg);
+  }
+
+  function map(obj, callback, thisArg) {
+    if (isArrayLike(obj)) {
+      return arrMap.call(obj, callback, thisArg);
+    }
+
+    var ret = clone(obj);
+
+    each(objKeys(ret), function(value, key) {
+      ret[key] = callback.apply(thisArg, arguments);
+    }, thisArg);
+
+    return ret;
+  }
+
+  function extend() {
+    return arrReduce.call(arguments, function(target, source) {
+      return objAssign(target, source);
+    });
+  }
+
+  function filter(obj, callback, thisArg) {
+    callback = callback || function(value) {
+      return value;
+    };
+
+    if (obj.length) {
+      return arrFilter.call(obj, callback, thisArg);
+    }
+
+    var ret = objCreate(objGetPrototypeOf(obj));
+
+    each(objKeys(obj).filter(function(key) {
+      return callback.call(thisArg, obj[key], key, obj);
+    }), function(key) {
+      ret[key] = obj[key];
+    });
+
+    return ret;
+  }
+
+  // array-like only
+  function unique(obj) {
+    return arrFilter.call(obj, function(val, i, arr) {
+      return (i <= arrIndexOf.call(arr, val));
+    });
+  }
+
+  function clone(obj, deep, callback) {
+    if (!(obj instanceof Object)) {
+      return obj;
+    }
+
+    var descriptors = {};
+
+    objGetOwnPropertyNames(obj).forEach(function(name) {
+      var prop = objGetOwnPropertyDescriptor(obj, name);
+
+      if (deep) {
+        prop.value = $.clone(prop.value);
+      }
+
+      descriptors[name] = prop;
+    });
+
+    return objCreate(objGetPrototypeOf(obj), descriptors);
+  }
+
+  function _posMod(a, b) {
+    a = a % b;
+    if (a < 0) {
+      a += b;
+    }
+    return a;
+  }
+
+  function get(obj, i) {
+    if (!isArrayLike(obj)) {
+      obj = objKeys(obj).sort();
+    }
+    return obj[_posMod(i, obj.length)];
+  }
+
+  function isArrayLike(obj) {
+    return obj.length !== undefined;
+  }
+
+  function last(obj) {
+    return get(obj, -1);
+  }
+
+  function first(obj) {
+    return get(obj, 0);
+  }
+
+  // array-like only
+  function flatten(obj, deep) {
+    var ret = [];
+
+    arrForEach.call(obj, function(value) {
+      arrForEach.call(value, function(subValue) {
+        ret.push(subValue);
+      });
+    });
+
+    return ret;
+  }
+
+  function values(obj) {
+    return objKeys(obj).map(function(key) {
+      return obj[key];
+    });
+  }
+
+  // XXX
+  function toArrayLike(obj) {
+    if (isArrayLike(obj)) {
+      return obj;
+    }
+
+    var ret = objCreate(null);
+
+    ret[0] = obj;
+    ret.length = 1;
+
+    return ret;
+  }
+
+  function toArray(obj) {
+    return arrSlice.call(obj, 0);
+  }
+
+  // XXX
+  function bindRight() {
+    var self = this;
+    var args = arrProto.slice.call(arguments);
+    return function() {
+      return self.apply(this, arrProto.slice.call(arguments).concat(args));
+    };
+  }
+
+  // array-like only
+  function difference(arr) {
+    var rest = flatten(arrSlice.call(arguments, 1));
+
+    return $.filter(arr, function(value) {
+      return !contains(rest, value);
+    });
+  }
+
+  // array-like only
+  function contains(arr, item) {
+    return ~arrIndexOf.call(arr, item);
+  }
+
+  // array-like only
+  function merge() {
+    return arrConcat.apply([], map(arguments, function(value) {
+      if (!arrIsArray(value) && isArrayLike(value)) {
+        value = toArray(value);
+      }
+      return value;
+    }));
+  }
+
+  return {
+    every: every,
+    some: some,
+    map: map,
+    each: each,
+    clone: clone,
+    unique: unique,
+    filter: filter,
+    extend: extend,
+    last: last,
+    first: first,
+    get: get,
+    flatten: flatten,
+    isArrayLike: isArrayLike,
+    toArrayLike: toArrayLike,
+    toArray: toArray,
+    values: values,
+    bindRight: bindRight,
+    indexOf: indexOf,
+    difference: difference,
+    contains: contains,
+    merge: merge
+  };
+})());
